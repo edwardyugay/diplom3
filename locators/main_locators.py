a22b61a077e7c1e2544646df29467719a534f7ac
EDUARD YUGAY <edwardehwa@gmail.com>
2025-05-22 22:18:14 +0900

    Обновлены локаторы и тест восстановления пароля по ревью

--- locators/main_locators.py
+++ locators/main_locators.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
 
 class MainLocators:
-    CONSTRUCTOR_BUTTON = (By.XPATH, "//p[text()='Конструктор']")
-    FEED_BUTTON = (By.XPATH, "//p[text()='Лента Заказов']")
-    INGREDIENT_ITEM = (By.CLASS_NAME, "burger-ingredient")
-    MODAL_CLOSE_BUTTON = (By.CLASS_NAME, "modal-close-icon")
-    CONSTRUCTOR_TAB = (By.XPATH, "//h1[text()='Соберите бургер']")
-    FEED_TAB = (By.XPATH, "//h1[text()='Лента заказов']")
-    INGREDIENT_COUNTER = (By.CLASS_NAME, "counter")
+    CONSTRUCTOR_BUTTON   = (By.XPATH, "//p[text()='Конструктор']")
+    FEED_BUTTON          = (By.XPATH, "//p[text()='Лента Заказов']")
+    INGREDIENT_ITEM      = (By.CLASS_NAME, "burger-ingredient")
+    MODAL_CLOSE_BUTTON   = (By.CLASS_NAME, "modal-close-icon")
+    CONSTRUCTOR_TAB      = (By.XPATH, "//h1[text()='Соберите бургер']")
+    FEED_TAB             = (By.XPATH, "//h1[text()='Лента заказов']")
+    INGREDIENT_COUNTER   = (By.CLASS_NAME, "counter")
